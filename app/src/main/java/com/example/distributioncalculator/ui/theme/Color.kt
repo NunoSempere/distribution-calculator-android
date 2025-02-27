a0776eb0026b798d79de1e86cf473a7ecd40dcb3
NunoSempere <nuno.semperelh@protonmail.com>
2025-02-27 14:53:44 -0300

    get decimals working, friendly colors

--- app/src/main/java/com/example/distributioncalculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/distributioncalculator/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Calculator button colors
+val OperationColor = Color(0xFF2196F3)  // Blue
+val NumberColor = Color(0xFF4CAF50)     // Green
+val UnitColor = Color(0xFFFF9800)       // Orange
+val CommandColor = Color(0xFF9C27B0)    // Purple
+val EqualColor = Color(0xFFE91E63)      // Pink
